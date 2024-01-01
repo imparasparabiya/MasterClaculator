1025525d0c9038bd3c2f4fa9e84504629a73de4e
imparasparabiya <parasparabiya@gmail.com>
2024-01-01 10:55:55 -0800

    Update 1-1-24

--- app/src/main/java/com/example/masterclaculator/IntrestcalcActivity.java
+++ app/src/main/java/com/example/masterclaculator/IntrestcalcActivity.java
@@ -1,11 +1,12 @@
 package com.example.masterclaculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -14,6 +15,7 @@ public class IntrestcalcActivity extends AppCompatActivity {
     TextInputEditText edtpamount, edtinterest, edtyears;
     Button btncalc;
 
+    ImageView backarrow;
     EditText edttotal;
 
     @Override
@@ -26,13 +28,31 @@ public class IntrestcalcActivity extends AppCompatActivity {
         edtyears = findViewById(R.id.edtyears);
         edttotal = findViewById(R.id.edttotal);
         btncalc = findViewById(R.id.btncalc);
+        backarrow = findViewById(R.id.backarrow);
+
+        backarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         btncalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                double principal = Double.parseDouble(edtpamount.getText().toString());
+                double interest = Double.parseDouble(edtinterest.getText().toString());
+                double years = Double.parseDouble(edtyears.getText().toString());
+                double finaltotal;
 
 
+                double totali = (principal * interest * years) / 100;
+
+
+                finaltotal = principal + totali;
+
+                edttotal.setText(String.format("%.2f", finaltotal));
             }
         });
     }
--- app/src/main/java/com/example/masterclaculator/SchoolResultActivity.java
+++ app/src/main/java/com/example/masterclaculator/SchoolResultActivity.java
@@ -3,12 +3,53 @@ package com.example.masterclaculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SchoolResultActivity extends AppCompatActivity {
+
+    ImageView backarrow;
+
+    TextInputEditText edttotalmarks, edtyourmarks;
+
+    Button btncalculate;
+
+    EditText edtresult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school_result);
+
+        backarrow = findViewById(R.id.backarrow);
+        edttotalmarks = findViewById(R.id.edttotalmarks);
+        edtyourmarks = findViewById(R.id.edtyourmarks);
+        btncalculate = findViewById(R.id.btncalculate);
+        edtresult = findViewById(R.id.edtresult);
+
+        backarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btncalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double totalmark = Double.parseDouble(edttotalmarks.getText().toString());
+                double yourmark = Double.parseDouble(edtyourmarks.getText().toString());
+
+                totalmark = totalmark / 100;
+
+                double result = yourmark / totalmark;
+
+                edtresult.setText(String.format("%.2f%%", result));
+            }
+        });
     }
 }
